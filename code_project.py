68d4bc4aa5cfe2358589de77af4d70f63b27d7a4
Tereza Kucerova <35284368@fsv.cuni.cz>
2025-06-03 10:37:58 +0200

    loading jsons, len

--- code_project.py
+++ code_project.py
@@ -1,12 +1,12 @@
 import pandas as pd
 import json as js
-
+import os
+print(os.getcwd())
 # Load data from JSON files
-with open('data_kosik.json', 'r', encoding='utf-8') as file, open('data_rohlik.json', 'r', encoding='utf-8') as file2:
+with open('/data_kosik.json', 'r', encoding='utf-8') as file, open('rohlik_dairy_products_multi_cat.json', 'r', encoding='utf-8') as file2:
     data_kosik = js.load(file)
-    #data_rohlik = js.load(file2)
+    data_rohlik = js.load(file2)
 
 
-    #print("Rohlik data:", rohlik.shape)
     print("Kosik data:", len(data_kosik))
-    #print("Rohlik data:", len(data_rohlik))
+    print("Rohlik data:", len(data_rohlik))
