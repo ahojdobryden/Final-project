507fb9b0deab4fbd06e1fc0ee36126851cbf5542
Tereza Kucerova <35284368@fsv.cuni.cz>
2025-06-03 17:24:34 +0200

    loading jsons to my test branch

--- code_project.py
+++ code_project.py
@@ -3,10 +3,16 @@ import json as js
 import os
 print(os.getcwd())
 # Load data from JSON files
-with open('/data_kosik.json', 'r', encoding='utf-8') as file, open('rohlik_dairy_products_multi_cat.json', 'r', encoding='utf-8') as file2:
+with open('data_kosik_subcats.json', 'r', encoding='utf-8') as file, open('rohlik_dairy_products_multi_cat.json', 'r', encoding='utf-8') as file2:
     data_kosik = js.load(file)
     data_rohlik = js.load(file2)
 
 
     print("Kosik data:", len(data_kosik))
     print("Rohlik data:", len(data_rohlik))
+
+    set_kosik = set()
+    for item in data_kosik:
+        set_kosik.add(item['name'])
+
+    print("Unique Kosik items:", len(set_kosik))
